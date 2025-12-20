a72fc1c0e35bb536b605a1def3d0d006cdad94f8
kacper <kacper.dlugosz125@gmail.com>
2025-12-20 14:57:20 +0100

    feat: add postgresql database configuration

---
+++ terraform/modules/database/01_main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_postgresql_flexible_server" "psqlflexibleserver" {
+  name                          = "psql-${var.workload}-${var.environment}-${var.location}-001"
+  resource_group_name           = var.rg_name
+  location                      = var.rg_location
+  version                       = "12"
+  delegated_subnet_id           = var.db_snet_id
+  private_dns_zone_id           = var.db_private_dns_zone_id
+  public_network_access_enabled = false
+  administrator_login           = "superuser1234"
+  administrator_password        = "superuserpassword1234"
+  zone                          = "1"
+
+  storage_mb   = 32768
+  storage_tier = "P4"
+
+  sku_name   = "B_Standard_B1ms"
+  depends_on = [var.db_private_link]
+
+}
