d3089f02a8c8200e37704ccba481754625d2eae2
kacper <kacper.dlugosz125@gmail.com>
2025-12-23 15:49:10 +0100

    feat: add jumpbox VM resource configuration

--- terraform/modules/jumpbox_vm/01_main.tf
+++ terraform/modules/jumpbox_vm/01_main.tf
@@ -9,3 +9,32 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
   }
 }
+
+
+resource "azurerm_linux_virtual_machine" "vm_jumpbox" {
+  name                = "vm-jumpbox-${var.workload}-${var.environment}-${var.location}-001"
+  location            = var.location
+  resource_group_name = var.rg_name
+  size                = "Standard_D2s_v3"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("${var.home}/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
