933cafb3cc772d27b6c651c60071ca884f8144ab
kacper <kacper.dlugosz125@gmail.com>
2025-12-23 15:56:07 +0100

    feat: add variables for jumpbox network security group configuration

---
+++ terraform/modules/nsg/02_variables.tf
@@ -0,0 +1,29 @@
+variable "workload" {
+  description = "Name of workload"
+  type = string
+}
+
+variable "environment" {
+  description = "Type of enviromet"
+  type = string
+}
+
+variable "location" {
+  description = "Resource location"
+  type = string
+}
+
+variable "rg_name" {
+  description = "Name of rg"
+  type = string
+}
+
+variable "rg_location" {
+  description = "Location of rg"
+  type = string
+}
+
+variable "jumpbox_snet_id" {
+  description = "Id of subnet where jumpbox vm is configured"
+  type = string
+}
