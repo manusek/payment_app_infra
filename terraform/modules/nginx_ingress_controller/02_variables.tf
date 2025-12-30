edbdbc992150b82490b4a5a9d7905bf13ff65eb6
kacper <kacper.dlugosz125@gmail.com>
2025-12-30 23:29:30 +0100

    feat: add variables to nginx controller module

---
+++ terraform/modules/nginx_ingress_controller/02_variables.tf
@@ -0,0 +1,19 @@
+variable "workload" {
+  description = "Name of workload"
+  type = string
+}
+
+variable "environment" {
+  description = "Type of enviromet"
+  type = string
+}
+
+variable "location" {
+  description = "Resource location"
+  type = string
+}
+
+variable "aks_cluster_id" {
+  description = "Id of AKS cluster"
+  type = string
+}
